ae752c03f2710fb524ec66b26623f552784e38c5
chirag-paypal <chiraggupta@paypal.com>
2017-09-15 10:52:45 +0530

    initial commmit

---
+++ StartingPoint.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class StartingPoint {
+	public static void main(String args[]){
+		Dog cg = new Dog();
+		Scanner inputvar = new Scanner(System.in);
+		int finish = inputvar.nextInt();
+		
+		Scanner inputword = new Scanner(System.in);
+		String words = inputword.nextLine();
+		while(finish > 0){
+//			cg.bark();
+			System.out.println(words);
+			finish -=1;
+		}
+			
+	}
+	
+}
